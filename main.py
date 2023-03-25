a29ec1c4cc4d3d506224892951dc33c440d046ae
ErmoshinMaks <101397201+ErmoshinMaks@users.noreply.github.com>
2023-03-26 01:13:28 +0300

    Commit after fixing the size bug

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
 import cv2
 import numpy as np
 import os
+
 def create_borders(path:str,size = 5):
     img = cv2.imread(path, cv2.IMREAD_COLOR)
-    half = size//2
-    result_img = img.copy()[half:img.shape[0]-half, half:img.shape[1]-half]
+    print(f"Size before processing: {img.shape}")
+
+    result_img = img.copy()
+    half = size // 2
+
+    for i in range(half):
+        img = np.insert(img, 0, [(-1,-1,-1) for i in range(img.shape[0])], axis=1)
+        img = np.insert(img, img.shape[1], [(-1,-1,-1) for i in range(img.shape[0])], axis=1)
+
+        img = np.insert(img, 0, [(-1,-1,-1) for i in range(img.shape[1])], axis=0)
+        img = np.insert(img, img.shape[0], [(-1,-1,-1) for i in range(0, img.shape[1])], axis=0)
+
     print("Processing..")
     for i in range(half,img.shape[0]-half):
         for j in range(half,img.shape[1]-half):
@@ -14,7 +25,7 @@ def create_borders(path:str,size = 5):
             flag = False
             for k in range(crop_img.shape[0]):
                 for n in range(crop_img.shape[1]):
-                    if not(np.array_equal(crop_img[k,n],main_color)):
+                    if not(np.array_equal(crop_img[k,n],main_color)) and not(np.array_equal(crop_img[k,n],(-1,-1,-1))):
                         count += 1
                     if count >= 2:
                         flag = True
@@ -22,10 +33,14 @@ def create_borders(path:str,size = 5):
             if flag:
                 result_img[i-half,j-half] = (0,0,0)
     print("Done!")
+    print(f"Size after processing: {result_img.shape}")
+
 
     cv2.imwrite("./out/" + os.path.basename(path).split('.')[0] + ".png", result_img)
     cv2.imshow("AFTER", result_img)
-    cv2.imshow("BEFORE", img)
+
+    img_before = cv2.imread(path, cv2.IMREAD_COLOR)
+    cv2.imshow("BEFORE", img_before)
 
 
     cv2.waitKey(0)
@@ -33,4 +48,4 @@ def create_borders(path:str,size = 5):
     cv2.destroyAllWindows()
 
 
-create_borders("./in/00001.png")
+create_borders("./in/00002.png")
